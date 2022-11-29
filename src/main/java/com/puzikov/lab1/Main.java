dd11f925d1945f2efcbc88ef3360ef6eb3ae8364
mechigurov <vip.moneta95@mail.ru>
2022-11-30 01:59:47 +0700

    created main file

--- src/main/java/com/puzikov/lab1/Main.java
+++ src/main/java/com/puzikov/lab1/Main.java
@@ -2,6 +2,6 @@ package com.puzikov.lab1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Леха, привет!\nКак дела?\nОсвоил git?");
     }
 }
