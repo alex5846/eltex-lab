5a6ebcbe3840942fcb755bf76c85bda29f3d0dc9
aapuzikov <puzikov_alex00@mail.ru>
2022-12-09 00:23:05 +0700

    Начал изучать java

--- src/main/java/com/puzikov/lab1/Main.java
+++ src/main/java/com/puzikov/lab1/Main.java
@@ -3,5 +3,6 @@ package com.puzikov.lab1;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Леха, привет!\nКак дела?\nОсвоил git?");
+        System.out.println("Миха, привет, git освоил, почти...");
     }
 }
