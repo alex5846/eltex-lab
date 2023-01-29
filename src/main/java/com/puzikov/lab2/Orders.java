99e24439c87d8e55d1926e409ac9d3df5d321a3d
aapuzikov <puzikov_alex00@mail.ru>
2023-01-29 23:13:28 +0700

    Реализован конструктор с параметрами и без параметров

--- src/main/java/com/puzikov/lab2/Orders.java
+++ src/main/java/com/puzikov/lab2/Orders.java
@@ -1,5 +1,23 @@
 package com.puzikov.lab2;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
 // Заказы. Объединение списков заказов
 public class Orders {
 
+    private List orders;                                            // Коллекция для хранения объектов в классе Orders (Хранит в себе Order)
+    private Map dateOrder;                                          // Коллекция для хранения объектов по времени создания
+
+    public Orders() {
+        this.orders = new ArrayList();
+        this.dateOrder = new TreeMap();
+    }
+
+    public Orders(List orders, Map createTime) {
+        this.orders = orders;
+        this.dateOrder = createTime;
+    }
 }
+
