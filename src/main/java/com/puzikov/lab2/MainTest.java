d4880c651cc97edaea3a70611ad0a1f6d179eed9
aapuzikov <puzikov_alex00@mail.ru>
2023-02-02 00:38:30 +0700

    Тестовый main

---
+++ src/main/java/com/puzikov/lab2/MainTest.java
@@ -0,0 +1,33 @@
+package com.puzikov.lab2;
+
+import java.util.*;
+
+public class MainTest {
+    public static void main(String[] args) {
+        // Создать объекты наследников, добавить их в корзину и вывести ORDER в консоль
+        Phone phone = new Phone();
+        Tablet tablet = new Tablet();
+        Smartphone smartphone = new Smartphone();
+        phone.create();
+        smartphone.create();
+        tablet.create();
+        Credentials user = new Credentials("Chigyrov", "Mihail", "Evgenevich", "miha123@mail.ru");
+        ShoppingCart cart = new ShoppingCart();
+        cart.addInCart(smartphone);
+        cart.addInCart(phone);
+        cart.addInCart(tablet);
+
+        Order order = new Order(user, cart);
+        Map<Integer, Date> createTime = new HashMap<>();
+        Date createTime2 = new Date(System.currentTimeMillis());
+        createTime.put(1, createTime2);
+        List orders = new ArrayList<>();
+        orders.add(order);
+        Orders orders2 = new Orders(orders, createTime);
+        System.out.println(order);
+        System.out.println(orders);
+// в orders создать метод offer2 на входе shoppingCart и данные пользователя (user), внутри метода создается заказ он добавляется в коллекцуию заказов и коллекцию времени заказов
+
+    }
+
+}
