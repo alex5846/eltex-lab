3e06322b9c2e3a2638ab148c09d26588cd7056db
aapuzikov <puzikov_alex00@mail.ru>
2023-03-05 14:30:50 +0700

    Сделан выход из меню.

--- src/main/java/com/puzikov/lab2/Main.java
+++ src/main/java/com/puzikov/lab2/Main.java
@@ -3,58 +3,60 @@ package com.puzikov.lab2;
 import java.util.*;
 
 
-// todo ты кое-что упустил: 6. Добавить для класса «заказ» поле «статус заказа» (в ожидании, обработан).
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
 
         Scanner scanner = new Scanner(System.in);
         Credentials user = new Credentials("Иванов", "Иван", "Иванович", "ivanov@mail.ru");
-        System.out.println("\nМеню:\n1.Телефон\n2.Смартфон\n3.Планшет\n");
-        int choice = scanner.nextInt();
+        int choice;
         ShoppingCart cart = new ShoppingCart();
         Phone[] phones = null;
-        //   Tablet[] tablets = null;
-        // Smartphone[] smartphones = null;
+        boolean incorrectChoice = true;
+        while (incorrectChoice) {
+            System.out.println("\nМеню:\n1.Телефон\n2.Смартфон\n3.Планшет\n0.Выход");
+            choice = scanner.nextInt();
 
-        if (choice == 1) {
-            System.out.println("Введите желаемое кол-во товара: ");
-            int count = scanner.nextInt();
-            if (count >= 0) {
-                phones = new Phone[count];
-                for (int i = 0; i < count; i++) {
-                    phones[i] = new Phone();
-                    phones[i].create();
-                    //phones[i].read();
-                    cart.add(phones[i]);
+            if (choice == 1) {
+                incorrectChoice = false;
+                System.out.println("Введите желаемое кол-во товара: ");
+                int count = scanner.nextInt();
+                if (count >= 0) {
+                    phones = new Phone[count];
+                    for (int i = 0; i < count; i++) {
+                        phones[i] = new Phone();
+                        phones[i].create();
+                        cart.add(phones[i]);
+                    }
                 }
-                // System.out.println("Количество товара: " + Phone.counterObject);
-            }
-        } else if (choice == 2) {
-            System.out.println("Введите желаемое количество товара: ");
-            int count = scanner.nextInt();
-            if (count >= 0) {
-                Smartphone[] smartphone = new Smartphone[count];
-                for (int i = 0; i < count; i++) {
-                    smartphone[i] = new Smartphone();
-                    smartphone[i].create();
-                    //smartphone[i].read();
-                    cart.add(smartphone[i]);
+            } else if (choice == 2) {
+                incorrectChoice = false;
+                System.out.println("Введите желаемое количество товара: ");
+                int count = scanner.nextInt();
+                if (count >= 0) {
+                    Smartphone[] smartphone = new Smartphone[count];
+                    for (int i = 0; i < count; i++) {
+                        smartphone[i] = new Smartphone();
+                        smartphone[i].create();
+                        cart.add(smartphone[i]);
+                    }
                 }
-                System.out.println("Количество товара: " + Smartphone.counterObject);
-            }
-        } else if (choice == 3) {
-            System.out.println("Введите желаемое количество товара");
-            int count = scanner.nextInt();
-            if (count >= 0) {
-                Tablet[] tablet = new Tablet[count];
-                for (int i = 0; i < count; i++) {
-                    tablet[i] = new Tablet();
-                    tablet[i].create();
-                    tablet[i].read();
-                    cart.add(tablet[i]);
+            } else if (choice == 3) {
+                incorrectChoice = false;
+                System.out.println("Введите желаемое количество товара");
+                int count = scanner.nextInt();
+                if (count >= 0) {
+                    Tablet[] tablet = new Tablet[count];
+                    for (int i = 0; i < count; i++) {
+                        tablet[i] = new Tablet();
+                        tablet[i].create();
+                        cart.add(tablet[i]);
+                    }
                 }
-                //  System.out.println("Количество товара: " + Tablet.counterObject);
+            } else if (choice == 0) {
+                return;
+            } else {
+                System.out.println("Введено некорректное значение. Выберете верный пункт из меню");
             }
         }
         System.out.println("1. Оформить заказ\n2. Отмена");
@@ -67,9 +69,9 @@ public class Main {
 
             }
             orders.checkTime();
-            System.out.println("Статус заказа: "+orders.getOrders().get(0).getStatus());
-            System.out.println("Вы купили товары со следующими характеристиками: ");
-            cart.checkPurchase();
+            // System.out.println("Статус заказа: "+orders.getOrders().get(0).getStatus());
+            // System.out.println("Вы купили товары со следующими характеристиками: ");
+            orders.show();
         }
 
     }
